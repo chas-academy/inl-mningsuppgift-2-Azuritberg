f20fe2013ac69042bd42a4dbf644506108fbc9a0
Azuritberg <143940692+Azuritberg@users.noreply.github.com>
2026-04-18 17:10:12 +0200

    klar för inlämning

--- src/script.js
+++ src/script.js
@@ -1,27 +1,37 @@
 // Definera array av namn
-let people;
+let people = ["Bojana", "Malin", "Maja", "Sara","Hanna", "Lisa", "Nina"];
 
 
 export function initSearch(people) { // rör ej denna rad
 
   // Hämta HTML-element
+  const searchInput = document.getElementById("searchInput");
+  const searchBtn = document.getElementById("searchBtn");
+  const resultDisplay = document.getElementById("resultDisplay");
   
   // Lägg till en klickhändelse på "Sök"-knappen
   searchBtn.addEventListener("click",() => {
   
     // 1. Hämta texten i searchInput.value
+    const inputName = searchInput.value;
     
     // 2. Skapa en boolean för found
+    let found = false;
     
     // 3. Loopa igenom people-arrayen
-    
-    // 4. Jämför texten med varje namn i arrayen
-    
-    // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
-    
+    for (let person of people) {
+      // 4. Jämför texten med varje namn i arrayen
+      if (person === inputName) {
+      // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
+        resultDisplay.innerHTML = "Namn hittades: " + inputName;
+        found = true;
+      }
+    }
     // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen.
-    
-    });
+    if (!found) {
+      resultDisplay.innerHTML = "Namn hittades inte.";
+    }
+  });
 }
 
 
